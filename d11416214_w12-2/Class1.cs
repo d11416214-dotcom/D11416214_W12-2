329926b506abbf91e94f7e9da0b397588bf2e323
d11416214-dotcom <d11416214@gs.takming.edu.tw>
2026-06-05 12:06:36 +0800

    Add files via upload

---
+++ d11416214_w12-2/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public interface IName
+{
+    public string GetName(); 
+}
+public class Student:IName
+{
+    public string GetName()
+    {
+        return "Student";
+    }
+}
+public class Teacher : IName
+{
+    public string GetName()
+    {
+        return "Teacher";
+    }
+}
---
+++ d11416214_w12-2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace d11416214_w12_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IName n1;
+            string input=Console.ReadLine();
+            if (input == "1")
+            {
+                n1=new Student();
+            }
+            else
+            {
+                n1=new Teacher();
+            }
+            Console.WriteLine(n1.GetName());
+        }
+    }
+}
